210a7abe0a7515b17ea4573e1c44525e164a5690
olgaborisova <olga.borisova803@gmail.com>
2020-10-12 20:55:12 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2000000000;
+        int b = 500000000;
+        int total = a+b;
+        System.out.println(total);
+    }
+}
